5b3264cd8486b1cb8d3047c5716ddd0dceead375
nicojoy-gif <67021106+nicojoy-gif@users.noreply.github.com>
2022-06-21 20:36:37 -0700

    Update introToJavascript.js

--- introToJavascript.js
+++ introToJavascript.js
@@ -1,4 +1,4 @@
 let name = 'Orji Onyinyechi';
 let height = 180;
 let country = 'Nigeria';
-console.log(`My name is ${name}, I'm ${height} cm in height and I'm from ${county}`);
+console.log(`My name is ${name}, I'm ${height} cm in height and I'm from ${country}`);
